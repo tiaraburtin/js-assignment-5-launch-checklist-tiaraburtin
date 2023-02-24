a2f657d0e4ef55a8f4193701f2d2220bcb98d689
Tiara <tburtin1019@gmail.com>
2023-02-24 17:35:17 -0600

    completed Valid Input and Form Submission

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,12 +17,42 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
-}
+    if (testInput === "" || testInput === null || testInput === 0) {
+        return "Empty";
+    } else if (!isNaN(parseInt(testInput))){ 
+        return "Is a number";
+    } return "Not a number";
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+    let pilotStatus = document.getElementId('pilotName')
+    let copilotStatus = document.getElementId('copilotName')
+    let fuelStatus = document.getElementId('fuelStatus')
+    let launchStatus = document.getElementId('launchStatus')
+    let cargoStatus = document.getElementId('cargoStatus')
+
+    if (validateInput(pilotStatus) === 'Empty' || validateInput(copilotStatus) === 'Empty' || validateInput(fuelStatus) === 'Empty' || validateInput(launchStatus) === 'Empty' || validateInput(launchStatus) === 'Empty' || validateInput(cargoStatus)=== 'Empty') {
+        window.alert('All fields are required!')
+            }
+
+    if (validateInput(pilotStatus) === 'Not a number' || validateInput(copilot) === 'Not a number' || validateInput(fuelStatus) === 'Not a number' || validateInput(launchStatus) === 'Not a number' || validateInput(cargoStatus) === "Not a number") {
+        window.alert('Make sure to enter valid information for each field!')
+             }   
+
+        else {
+            pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch.`
+            copilotStatus.innerHTML = `Co-Pilot ${copilot} is ready for launch.`
+
+        }
+    }  
+
+    if (Number(fuelStatus) < 10000) {
+        fuelStatus.innerHTML = 'There is not enough fuel for the journey.'
+        launchStatus.innerHTML = 'Shuttle not ready for launch.'
+    } else if  (Number(cargoStatus) > 10000) {
+        cargoStatus.innerHTML = 'There is too much mass for the shuttle to take off.'
+        launchStatus.innerHTML = 'Shuttle not ready for launch.'
+    }
 
 async function myFetch() {
     let planetsReturned;
